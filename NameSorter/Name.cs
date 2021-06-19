ffb5b87f164034b3b94a985fc97d0ed7686c2fcb
Aruni Kariyawasamuni <arunik.27@hotmail.com>
2021-06-19 16:30:50 +1000

    Read the input file and sort the names

---
+++ NameSorter/Name.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NameSorter
+{
+    public class Name
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public bool ValidateLastNameExist(string fullName)
+        {
+            var nameParts = fullName.Trim().Split(' ');
+            // var lastName = nameParts.LastOrDefault();
+
+            if (nameParts.Length >= 2)
+                return true;
+            else
+                return false;
+        }
+
+        public bool ValidString(string fullName)
+        {
+            var nameParts = fullName.Trim().Split(' ');
+            
+            if (Regex.IsMatch(fullName, @"^[a-zA-Z ]+$"))
+                return true;
+
+            else
+                return false;
+        }
+
+    }
+}
--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -9,9 +9,32 @@ namespace NameSorter
     {
         static void Main(string[] args)
         {
+
+            try
+            {               
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Please enter a file name");
+                    
+                }
+                Program program = new Program();
+                program.SortingNames();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void SortingNames()
+        {
             try
             {
-                Console.Write("name-sorter :");
+                Name nameObj = new Name();
+
+               // Console.Write("name-sorter :");
                 var fileName = Console.ReadLine();
 
                 // Check the give file is exist
@@ -20,63 +43,109 @@ namespace NameSorter
                     FileStream fileStream = new FileStream(fileName, System.IO.FileMode.Open);
 
                     Dictionary<String, String> names = new Dictionary<String, String>();
-                    List<FullName> NameList = new List<FullName>();
 
+                    List<Name> NameList = new List<Name>();
+                    List<Name> InvalidNameList = new List<Name>();
                     using (StreamReader reader = new StreamReader(fileStream))
                     {
                         string line = "";
                         while ((line = reader.ReadLine()) != null)
                         {
-                            var nameParts = line.Split(' ');
-                            var lastName = nameParts.LastOrDefault();
+                            // Check the read line has only characters
+                            if (nameObj.ValidString(line))
+                            {
 
-                            var firstName = string.Join(" ", nameParts.Take(nameParts.Length - 1));
-                           
-                                FullName fullNameObj = new FullName();
-                                fullNameObj.firstName = firstName;
-                                fullNameObj.lastName = lastName;
-                                NameList.Add(fullNameObj);                            
+                                // Check the read line has firstname and last name
+                                if (nameObj.ValidateLastNameExist(line))
+                                {
+                                    var nameParts = line.Split(' ');
+                                    var lastName = nameParts.LastOrDefault();
+
+                                    var firstName = string.Join(" ", nameParts.Take(nameParts.Length - 1));
+
+                                    Name fullNameObj = new Name();
+                                    fullNameObj.firstName = firstName;
+                                    fullNameObj.lastName = lastName;
+                                    NameList.Add(fullNameObj);
+                                }
+                                else
+                                {
+                                    //Save the invalid entries
+                                    Name fullNameObj = new Name();
+                                    fullNameObj.firstName = "Invalid entry -";
+                                    fullNameObj.lastName = line;
+                                    InvalidNameList.Add(fullNameObj);
+                                }
+                            }
+                            else
+                            {
+                                Name fullNameObj = new Name();
+                                fullNameObj.firstName = "Invalid characters -";
+                                fullNameObj.lastName = line;
+                                InvalidNameList.Add(fullNameObj);
+
+                            }
                         }
                     }
-                   
-                    string SortedPath = @".\sorted-names-list.txt";
 
-                    if (File.Exists(SortedPath))
-                    {
-                        File.Delete(SortedPath);
-                    }
 
-                    List<FullName> SortedList = NameList.OrderBy(o => o.lastName).ToList();
-                    for (int i = 0; i < SortedList.Count(); i++)
-                    {
-                        Console.WriteLine(SortedList[i].firstName.ToString() + " " + SortedList[i].lastName.ToString());
-                        using (StreamWriter sw = File.AppendText(SortedPath))
-                        {
-                            sw.WriteLine(SortedList[i].firstName.ToString() + " " + SortedList[i].lastName.ToString());
+                    Console.WriteLine($"{Environment.NewLine}Sorted Names list.{Environment.NewLine}");
+                    List<Name> SortedList = NameList.OrderBy(o => o.lastName).ToList();
+                    PrintNameList(SortedList);
+                    WriteNameList(SortedList);
 
-                        }
-                    }
+
+                    //print the  invalid entries
+                    if (InvalidNameList.Count() >= 1)
+                        Console.WriteLine($"{Environment.NewLine}Skipped Names list. {Environment.NewLine}");
+                    PrintNameList(InvalidNameList);
+
                 }
                 else
                 {
-                    Console.WriteLine( "File does not exist.");
-                }                
+                    Console.WriteLine("File does not exist.");
+                }
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-
         }
 
-        public class FullName
+
+        public void PrintNameList(List<Name> NameList)
         {
-            public string firstName { get; set; }
-            public string lastName { get; set; }
+
+            for (int i = 0; i < NameList.Count(); i++)
+            {
+                Console.WriteLine(NameList[i].firstName.ToString() + " " + NameList[i].lastName.ToString());
+            }
+
         }
 
-        
+        public void WriteNameList(List<Name> NameList)
+        {
+
+            string SortedPath = @".\sorted-names-list.txt";
+
+            if (File.Exists(SortedPath))
+            {
+                File.Delete(SortedPath);
+            }
+
+            for (int i = 0; i < NameList.Count(); i++)
+            {
+                using (StreamWriter sw = File.AppendText(SortedPath))
+                {
+                    //write the  valid entries
+                    sw.WriteLine(NameList[i].firstName.ToString() + " " + NameList[i].lastName.ToString());
+
+                }
+            }
+
+        }
+
     }
-    
+
 }
